faaa815534f5faba5f38804821f958022f85a457
GabrielNarvadez <131564153+GabrielNarvadez@users.noreply.github.com>
2024-11-19 16:32:40 +0800

    Use maria db

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 // src/models/db.js
 
-const mysql = require('mysql2');
+const mariadb = require('mariadb');
 require('dotenv').config();
 
 console.log('DB_HOST:', process.env.DB_HOST);
@@ -8,19 +8,24 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_NAME:', process.env.DB_NAME);
 
-const db = mysql.createConnection({
+// Create a pool of connections (recommended for MariaDB)
+const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectionLimit: 5 // Limit to 5 concurrent connections
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('MySQL connection error:', err);
-    throw err;
-  }
-  console.log('MySQL connected...');
-});
+// Test the connection
+pool.getConnection()
+  .then(conn => {
+    console.log('Connected to MariaDB');
+    conn.release(); // Release the connection back to the pool
+  })
+  .catch(err => {
+    console.error('MariaDB connection error:', err);
+  });
 
-module.exports = db;
+// Export the pool for use in other parts of your app
+module.exports = pool;
